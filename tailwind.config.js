fd273411b902523d8cd9a2f519ceba6e068a9cc1
gyahury <rudgns1243@naver.com>
2024-05-06 17:44:41 +0900

    chore: apply screen media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      screens: {
+        'minimum-height': {'raw': '(max-height: 350px)'}, 
+        'minimum-width': {'raw': '(max-width: 345px)'}, 
+      },
       fontFamily: {
         sans: ['Noto Sans JP', 'sans-serif'],
       },
