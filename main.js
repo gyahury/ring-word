2d7d66eaebf2f33bb598f6302c0c3faacad2c24d
gyahury <rudgns1243@naver.com>
2024-05-06 19:24:34 +0900

    chore: add element id

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import './style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const wordDiv = document.getElementById('word');
+  const curWordCountSpan = document.getElementById('curWordCount');
+  const allWordCountSpan = document.getElementById('allWordCount');
+  const prevButton = document.getElementById('prevButton');
+  const flipButton = document.getElementById('flipButton');
+  const nextButton = document.getElementById('nextButton');
   let data = [];
+  
   fetch(import.meta.env.VITE_API_URL + '/gyahury/words.json')
     .then((response) => response.json())
     .then((json) => {
