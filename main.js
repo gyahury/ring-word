b918d9de21dc9bf81d6b459d7d569c36da6f36f9
gyahury <rudgns1243@naver.com>
2024-05-06 18:59:03 +0900

    feat: apply fetch api - update .gitignore

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-import './style.css'
+import './style.css';
 
-
-
-
+document.addEventListener('DOMContentLoaded', () => {
+  let data = [];
+  fetch(import.meta.env.VITE_API_URL + '/gyahury/words.json')
+    .then((response) => response.json())
+    .then((json) => {
+      data = json.words;
+      console.log(data);
+    })
+    .catch((error) => console.error(error));
+});
