7a9e46ab115b3a9fa16828e5292082e189ad9ae6
gyahury <gyahury@users.noreply.github.com>
2024-05-15 04:35:45 +0000

    deploy: 073ec3d1f5a0e501a25e2c9879a2acc492310909

---
+++ word-page/index.js
@@ -0,0 +1,7 @@
+import '../style.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const nickname = urlParams.get('nickname');
+    alert(nickname);
+});
