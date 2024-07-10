66820a64dc0d43f38e8adc6b5588f7b476551b46
gyahury <rudgns1243@naver.com>
2024-07-10 22:43:47 +0900

    refactor: error logic updated

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputValue = document.getElementById('nickname').value.trim();
     if (inputValue) {
       const requestUrl = `${import.meta.env.VITE_API_URL}/${inputValue}/words.json`;
-      alert(requestUrl)
       fetch(requestUrl, { method: 'HEAD' })
         .then((response) => {
           response.ok
